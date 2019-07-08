cc5725d87177dc5e3aa34cfe5fc40e116d04b7e4
Vitaliy <theyamshikov@yandex.ru>
2019-07-08 15:11:33 +0600

    style: clean code

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -9,7 +9,6 @@ use yii2rails\domain\enums\Driver;
  * 
  * @package yii2bundle\i18n\domain
  * @property-read \yii2bundle\i18n\domain\interfaces\repositories\RepositoriesInterface $repositories
- * @property-read \yii2bundle\i18n\domain\interfaces\services\DomainInterface $domain
  * @property-read \yii2bundle\i18n\domain\interfaces\services\LanguageInterface $language
  * @property-read \yii2bundle\i18n\domain\interfaces\services\EntityInterface $entity
  * @property-read \yii2bundle\i18n\domain\interfaces\services\FieldInterface $field
@@ -20,13 +19,11 @@ class Domain extends \yii2rails\domain\Domain {
 	public function config() {
 		return [
 			'repositories' => [
-				'domain' => Driver::ACTIVE_RECORD,
 				'entity' => Driver::ACTIVE_RECORD,
 				'field' => Driver::ACTIVE_RECORD,
 				'content' => Driver::ACTIVE_RECORD,
 			],
 			'services' => [
-				'domain',
 				'entity',
 				'field',
 				'content',
--- src/domain/interfaces/repositories/RepositoriesInterface.php
+++ src/domain/interfaces/repositories/RepositoriesInterface.php
@@ -8,7 +8,6 @@ namespace yii2bundle\i18n\domain\interfaces\repositories;
  * @package yii2bundle\i18n\domain\interfaces\repositories
  * 
  * @property-read \yii2bundle\i18n\domain\interfaces\repositories\ContentInterface $content
- * @property-read \yii2bundle\i18n\domain\interfaces\repositories\DomainInterface $domain
  * @property-read \yii2bundle\i18n\domain\interfaces\repositories\EntityInterface $entity
  * @property-read \yii2bundle\i18n\domain\interfaces\repositories\FieldInterface $field
  * @property-read \yii2bundle\i18n\domain\interfaces\repositories\LanguageInterface $language
